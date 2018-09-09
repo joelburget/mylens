4feaabb63c31eb529bae801322f6478edeb97d32
Joel Burget <joelburget@gmail.com>
2018-09-09 13:22:08 -0700

    Add `Prism` and notes.

--- src/Main.hs
+++ src/Main.hs
@@ -7,19 +7,72 @@ module Main where
 
 import Data.Complex
 import Data.Functor.Const
+import Data.Functor.Contravariant
 import Data.Functor.Identity
 import Data.Profunctor
 
 import Data.Profunctor.Unsafe ((#.))
 
-type Lens s t a b = forall   f. Functor f                 => (a -> f b) -> s -> f t
+type Lens      s t a b = forall   f. Functor f                    => (a -> f b) -> s -> f t
 
--- Generalize `->` to `(Profunctor p =>) p`.
-type Iso  s t a b = forall p f. (Functor f, Profunctor p) => p a (f b)  -> p s (f t)
+type Traversal s t a b = forall   f. Applicative f                => (a -> f b) -> s -> f t
+
+type Iso       s t a b = forall p f. (Functor f, Profunctor p)    => p a (f b)  -> p s (f t)
+
+type Prism     s t a b = forall p f. (Applicative f, Choice p)    => p a (f b)  -> p s (f t)
+
+type Getter    s   a   = forall   f. (Functor f, Contravariant f) => (a -> f a) -> s -> f s
+
+type Optic p f s t a b =                                             p a (f b)  -> p s (f t)
+
+-- Traversal:
+--
+-- We strengthen `Functor` to `Applicative`. Why?
+--
+-- Clue: note that
+--
+-- traverse :: Applicative f => (a -> f b) -> t a -> f (t b)
+--           ~ Traversable f => Traversal (f a) (f b) a b
+--
+-- So, instead we can ask why does `traverse` require `Applicative`?
+--
+-- The instance for lists is informative:
+--
+-- instance Traversable [] where
+--   traverse _ []     = pure []
+--   traverse f (x:xs) = (:) <$> f x <*> traverse f xs
+--
+-- Iso:
+--
+-- We generalize `->` to `(Profunctor p =>) p`. Why?
+--
+-- One answer is that using `Exchange` (a `Profunctor`) allows us to reverse
+-- the `Iso` and extract its parts.
+--
+-- Prism:
+--
+-- Here `f` is an `Applicative` and `p` is a `Choice`. Why?
+--
+-- First of all:
+-- * Every `Prism`: Applicative f, Choice p
+--   is a valid          v            v
+--     `Traversal`: Applicative f
+--
+--   Here `Prism` adds `Choice p`, meaning TODO
+--
+-- * Every `Iso`:   Functor f,     Profunctor p
+--   is a valid           v             v
+--         `Prism`: Applicative f, Choice p
+--
+--   Here `Prism` strenthens the constraints:
+--   * `Functor` to `Applicative` because it doesn't touch exactly one position
+--   * `Profunctor` to `Choice` because TODO
+
 
 type Simple f s a = f s s a a
 type Lens' s a = Simple Lens s a
 type Iso'  s a = Simple Iso  s a
+type Getting r s a = (a -> Const r a) -> s -> Const r s
 
 iso :: (s -> a) -> (b -> t) -> Iso s t a b
 iso sa bt = dimap sa (fmap bt)
